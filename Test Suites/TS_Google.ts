3b74598a07741e5ec4795262249c6a5ca3f5c71f
tariqansari <tariq.ansarimd@cognizant.com>
2018-07-09 12:08:52 +0530

    Entire project commit and push

---
+++ Test Suites/TS_Google.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Google</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>944e1cd3-d1a3-425c-83dd-d30d6be0e846</testSuiteGuid>
+</TestSuiteEntity>
